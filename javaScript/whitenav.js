3c33786ce43d10ad1fdf349e139268f001018589
Justin  Tubay <justintubay@Justins-Air.lan1>
2020-02-12 00:38:22 -0500

    design

---
+++ javaScript/whitenav.js
@@ -0,0 +1,10 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("navBar").style.height = "4rem";
+  } else {
+    document.getElementById("navBar").style.height = "6rem";
+  }
+}
